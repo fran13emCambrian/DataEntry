d456251e4d708d8a8f3c987c466b946d224d0ce3
francisco <a00223609@mycambrian.ca>
2021-12-13 20:15:51 -0800

    Add project files.

---
+++ DataEntry/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DataEntry.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataEntry.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("DataForm");
+        }
+
+        [HttpPost]
+        public IActionResult DataForm(DataModel data)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Display", data);
+            }
+            else
+            {
+                return View("Display");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DataEntry/Models/DataModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataEntry.Models
+{
+    public class DataModel
+    {
+   
+        public string First_Name { get; set; }
+
+        public string Second_Name { get; set; }
+      
+        public string Address { get; set; }
+        public bool? Course { get; set; }
+    
+        public int Id_number { get; set; }
+    }
+}
